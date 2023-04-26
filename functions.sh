8baca0332c0b63bef6819fbbcb475fe9524c4f78
GerardoCMM <gerardo_cendejas_mendoza@ciencias.unam.mx>
2023-04-26 16:17:46 -0600

    Uploaded

--- functions.sh
+++ functions.sh
@@ -1,25 +1,3 @@
-function QualityControl () {
-
-	echo "#######Análisis de calidad con FASTQC y MultiQC#######"
-
-	#Análisis de calidad con fastqc
-	
-	fastqc $1_1.fastq || fastqc $1_1.fastq.gz
-
-	fastqc $1_2.fastq || fastqc $2_1.fastq.gz
-
-	# Resumen con MultiQC
-
-	multiqc .
-
-	rm *fastqc.*
-
-	# Descomprimir archivos
-
-	gunzip $1_1.fastq.gz $1_2.fastq.gz || echo "Los archivos ya están descomprimidos"
-
-}
-
 function CleanReads (){
 
 	echo "#######Limpiando las lecturas#######"
@@ -28,66 +6,67 @@ function CleanReads (){
 
 	# Nos movemos a la carpeta parts
 
-	cd parts
+	mkdir dedup qual compl trim clean
 
 	# Crea las carpetas necesarias
 
-	mkdir dedup qual compl trim norm
-
 	# Iteramos por cada parte y realizamos la limpieza
 
-	i=0
+	echo "### Quality control for $sample ###"
 
-	while [ $i -ne $2 ]
-	do
-	        i=$(($i+1))
-		j="0"
-		if [[ $i -lt 10 ]]
-		then
-		j="$j$i"
-		else
-		j=$i
-		fi
+	# Quitando lecturas duplicadas
 
-	        echo "### Quality control for part $i ###"
+	prinseq++ -verbose -fastq $1_1.fastq -fastq2 $1_2.fastq -derep -rm_header -out_good ./dedup/$1_dedup_1.fastq -out_good2 ./dedup/$1_dedup_2.fastq -out_single ./dedup/$1_dedup_singletons_1.fastq -out_single2 ./dedup/$1_dedup_singletons_2.fastq -out_bad /dev/null -out_bad2 /dev/null
 
-	        # Quitando lecturas duplicadas
+	# Filtrado de secuencias con mean_score mínimo de $2 y tamaño mínimo de $3
 
-			prinseq-lite.pl -verbose -fastq $1_1.part-"$j".fastq -fastq2 $1_2.part-"$j".fastq -derep 1 -derep_min 6 -log ./dedup/$1_part"$i"_dedup - no_qual_header -out_good ./dedup/$1_part"$i"_dedup -out_bad null
+	prinseq++ -verbose -fastq ./dedup/$1_dedup_1.fastq -fastq2 ./dedup/$1_dedup_2.fastq -min_qual_mean $2 -rm_header -min_len $3 -out_good ./qual/$1_qual_1.fastq -out_good2 ./qual/$1_qual_2.fastq -out_single ./qual/$1_qual_singletons_1.fastq -out_single2 ./qual/$1_qual_singletons_2.fastq -out_bad /dev/null -out_bad2 /dev/null
 
-			# Filtrado de secuencias con mean_score mínimo de $3 y tamaño mínimo de $4
+	# Remoción con base en la complejidad para agilizar proceso
 
-			prinseq-lite.pl -verbose -fastq ./dedup/$1_part"$i"_dedup_1.fastq -fastq2 ./dedup/$1_part"$i"_dedup_2.fastq -min_qual_mean $3 -log ./qual/$1_part"$i"_qual - no_qual_header -min_len $4 -out_good ./qual/$1_part"$i"_qual -out_bad null
+	prinseq++ -verbose -fastq ./qual/$1_qual_1.fastq -fastq2 ./qual/$1_qual_2.fastq -lc_entropy=0.7 -rm_header -out_good ./compl/$1_compl_1.fastq -out_good2 ./compl/$1_compl_2.fastq -out_single ./compl/$1_compl_singletons_1.fastq -out_single2 ./compl/$1_compl_singletons_2.fastq -out_bad /dev/null -out_bad2 /dev/null
 
-			# Remoción con base en la complejidad para agilizar proceso
+	# Trimming con min_qual $2 y min_length $3
 
-			prinseq-lite.pl -verbose -fastq ./qual/$1_part"$i"_qual_1.fastq -fastq2 ./qual/$1_part"$i"_qual_2.fastq -lc_threshold 70 -lc_method entropy -log ./compl/$1_part"$i"_compl -no_qual_header -out_good ./compl/$1_part"$i"_compl -out_bad null
+	prinseq++ -verbose -fastq ./compl/$1_compl_1.fastq -fastq2 ./compl/$1_compl_2.fastq -trim_qual_right=$2 -trim_qual_left=$2 -trim_qual_window 1 -trim_qual_step 1 -min_len $3 -rm_header -out_good ./trim/$1_trim_1.fastq -out_good2 ./trim/$1_trim_2.fastq -out_single ./trim/$1_trim_singletons_1.fastq -out_single2 ./trim/$1_trim_singletons_2.fastq -out_bad /dev/null -out_bad2 /dev/null
 
-			# Trimming con min_qual $3 y min_length $4
+	# Normalización de profundidad
 
-			prinseq-lite.pl -verbose -fastq ./compl/$1_part"$i"_compl_1.fastq -fastq2 ./compl/$1_part"$i"_compl_2.fastq -trim_qual_right $3 -trim_qual_type min - trim_qual_rule lt -trim_qual_window 1 -trim_qual_step 1 -log ./trim/$1_part"$i"_trim - min_len $4 -no_qual_header -out_good ./trim/$1_part"$i"_trim -out_bad null
+	bbnorm.sh in=./trim/$1_trim_1.fastq in2=./trim/$1_trim_2.fastq out=./clean/$1_clean_1.fastq out2=./clean/$1_clean_2.fastq target=100
 
-			# Normalización de profundidad
-
-			bbnorm.sh in=./trim/$1_part"$i"_trim_1.fastq in2=./trim/$1_part"$i"_trim_2.fastq out=./norm/$1_part"$i"_norm_1.fastq out2=./norm/$1_part"$i"_norm_2.fastq target=100
-	done
 
 	echo "#####Analisis de calidad de singletons#####"
 
+	cat ./dedup/*singletons* >> ./dedup/single_reads_dedup.fastq || echo "No se recuperaron singletons en la deuplicacion"
+
+	prinseq++ -verbose -fastq ./dedup/single_reads_dedup.fastq  -min_qual_mean $2 -rm_header -min_len $3 -out_good ./qual/single_reads_qual.fastq -out_bad /dev/null || echo "No se puede realizar el filtrado de calidad sobre singletons inexistentes"
+
 	cat ./qual/*singletons* >> ./qual/single_reads_qual.fastq || echo "No se recuperaron singletons en el filtro de calidad"
 
-	prinseq-lite.pl -verbose -fastq ./qual/single_reads_qual.fastq -lc_threshold 70 -lc_method entropy -log ./compl/single_reads_compl -no_qual_header -out_good ./compl/single_reads_compl -out_bad null || echo "No se puede realizar el filtrado de complejidad sobre singletons inexistentes"
+	prinseq++ -verbose -fastq ./qual/single_reads_qual.fastq -lc_entropy=0.7 -rm_header -out_good ./compl/single_reads_compl -out_bad null || echo "No se puede realizar el filtrado de complejidad sobre singletons inexistentes"
 
 	cat ./compl/*singletons* >> ./compl/single_reads_compl.fastq || echo "No se recuperaron singletons en el filtro de calidad"
 
-	prinseq-lite.pl -verbose -fastq ./compl/single_reads_compl.fastq -trim_qual_right $3 -trim_qual_type min - trim_qual_rule lt -trim_qual_window 1 -trim_qual_step 1 -log ./trim/single_reads_trim - min_len $4 -no_qual_header -out_good ./trim/single_reads_trim -out_bad null || echo "No se puede realizar el trimming sobre singletons inexistentes"
+	prinseq++ -verbose -fastq ./compl/single_reads_compl.fastq -trim_qual_right=$2 -trim_qual_left=$2 -trim_qual_window 1 -trim_qual_step 1 -min_len $3 -rm_header -out_good ./trim/single_reads_trim -out_bad null || echo "No se puede realizar el trimming sobre singletons inexistentes"
 
-	bbnorm.sh in=./trim/single_reads_trim.fastq out=./norm/singletons_norm.fastq target=100 || echo "No se obtuvieron singletons durante el análisis de calidad de esta muestra"
+	bbnorm.sh in=./trim/single_reads_trim.fastq out=./clean/singletons_clean.fastq target=100 || echo "No se obtuvieron singletons durante el análisis de calidad de esta muestra"
 
 	rm -rf dedup qual compl trim
 
-	rm *.fastq
+}
 
-	cd ..
+function HostMApping(){
+
+	mkdir unmapped
+
+	cd unmapped
+
+	echo  "Mapping pair reads to host"
+
+	STAR --runThreadN 16 --genomeDir $2 --sjdbGTFfile $2/*.gtf --sjdbOverhang 150 -- readFilesIn ../clean/$1_clean_1.fastq ../clean/$1_clean_2.fastq --outSAMtype BAM SortedByCoordinate --outSAMstrandField intronMotif
+
+	echo  "Mapping singletons to host"
+
+	STAR --runThreadN 16 --genomeDir $2 --sjdbGTFfile $2/*.gtf --sjdbOverhang 150 -- readFilesIn ../clean/singletons_clean.fastq --outSAMtype BAM SortedByCoordinate --outSAMstrandField intronMotif
 
 }
--- script.sh
+++ script.sh
@@ -18,14 +18,14 @@ min_qual=28 # Calidad mínima de las lecturas para la limpieza
 
 min_len=75 # Longitud mínima de las lecturas para la limpieza
 
-####### Análisis de calidad de las lecturas usando FastQC y MultiQC #######
-
-QualityControl $sample
-
-### Separar los archivos en partes más pequeñas y mover a propia carpeta ###
-
-SplitFiles $sample $n_parts
+genome= # Path to reference genome
 
 ### Limpieza de las lecturas ###
 
-CleanReads $sample $n_parts $min_qual $min_len
+CleanReads $sample $min_qual $min_len
+
+### Mapeo a host
+
+HostMapping $sample $genome
+
+
