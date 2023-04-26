471cd965cad5e97cffdea2d0d444d425ecf734a2
GerardoCMM <gerardo_cendejas_mendoza@ciencias.unam.mx>
2023-04-26 16:56:36 -0600

    Uploaded

--- functions.sh
+++ functions.sh
@@ -16,19 +16,19 @@ function CleanReads (){
 
 	# Quitando lecturas duplicadas
 
-	prinseq++ -verbose -fastq $1_1.fastq -fastq2 $1_2.fastq -derep -rm_header -out_good ./dedup/$1_dedup_1.fastq -out_good2 ./dedup/$1_dedup_2.fastq -out_single ./dedup/$1_dedup_singletons_1.fastq -out_single2 ./dedup/$1_dedup_singletons_2.fastq -out_bad /dev/null -out_bad2 /dev/null
+	prinseq++ -fastq $1_1.fastq -fastq2 $1_2.fastq -derep -rm_header -out_good ./dedup/$1_dedup_1.fastq -out_good2 ./dedup/$1_dedup_2.fastq -out_single ./dedup/$1_dedup_singletons_1.fastq -out_single2 ./dedup/$1_dedup_singletons_2.fastq -out_bad /dev/null -out_bad2 /dev/null
 
 	# Filtrado de secuencias con mean_score mínimo de $2 y tamaño mínimo de $3
 
-	prinseq++ -verbose -fastq ./dedup/$1_dedup_1.fastq -fastq2 ./dedup/$1_dedup_2.fastq -min_qual_mean $2 -rm_header -min_len $3 -out_good ./qual/$1_qual_1.fastq -out_good2 ./qual/$1_qual_2.fastq -out_single ./qual/$1_qual_singletons_1.fastq -out_single2 ./qual/$1_qual_singletons_2.fastq -out_bad /dev/null -out_bad2 /dev/null
+	prinseq++ -fastq ./dedup/$1_dedup_1.fastq -fastq2 ./dedup/$1_dedup_2.fastq -min_qual_mean $2 -rm_header -min_len $3 -out_good ./qual/$1_qual_1.fastq -out_good2 ./qual/$1_qual_2.fastq -out_single ./qual/$1_qual_singletons_1.fastq -out_single2 ./qual/$1_qual_singletons_2.fastq -out_bad /dev/null -out_bad2 /dev/null
 
 	# Remoción con base en la complejidad para agilizar proceso
 
-	prinseq++ -verbose -fastq ./qual/$1_qual_1.fastq -fastq2 ./qual/$1_qual_2.fastq -lc_entropy=0.7 -rm_header -out_good ./compl/$1_compl_1.fastq -out_good2 ./compl/$1_compl_2.fastq -out_single ./compl/$1_compl_singletons_1.fastq -out_single2 ./compl/$1_compl_singletons_2.fastq -out_bad /dev/null -out_bad2 /dev/null
+	prinseq++ -fastq ./qual/$1_qual_1.fastq -fastq2 ./qual/$1_qual_2.fastq -lc_entropy=0.7 -rm_header -out_good ./compl/$1_compl_1.fastq -out_good2 ./compl/$1_compl_2.fastq -out_single ./compl/$1_compl_singletons_1.fastq -out_single2 ./compl/$1_compl_singletons_2.fastq -out_bad /dev/null -out_bad2 /dev/null
 
 	# Trimming con min_qual $2 y min_length $3
 
-	prinseq++ -verbose -fastq ./compl/$1_compl_1.fastq -fastq2 ./compl/$1_compl_2.fastq -trim_qual_right=$2 -trim_qual_left=$2 -trim_qual_window 1 -trim_qual_step 1 -min_len $3 -rm_header -out_good ./trim/$1_trim_1.fastq -out_good2 ./trim/$1_trim_2.fastq -out_single ./trim/$1_trim_singletons_1.fastq -out_single2 ./trim/$1_trim_singletons_2.fastq -out_bad /dev/null -out_bad2 /dev/null
+	prinseq++ -fastq ./compl/$1_compl_1.fastq -fastq2 ./compl/$1_compl_2.fastq -trim_qual_right=$2 -trim_qual_left=$2 -trim_qual_window 1 -trim_qual_step 1 -min_len $3 -rm_header -out_good ./trim/$1_trim_1.fastq -out_good2 ./trim/$1_trim_2.fastq -out_single ./trim/$1_trim_singletons_1.fastq -out_single2 ./trim/$1_trim_singletons_2.fastq -out_bad /dev/null -out_bad2 /dev/null
 
 	# Normalización de profundidad
 
@@ -39,19 +39,19 @@ function CleanReads (){
 
 	cat ./dedup/*singletons* >> ./dedup/single_reads_dedup.fastq || echo "No se recuperaron singletons en la deuplicacion"
 
-	prinseq++ -verbose -fastq ./dedup/single_reads_dedup.fastq  -min_qual_mean $2 -rm_header -min_len $3 -out_good ./qual/single_reads_qual.fastq -out_bad /dev/null || echo "No se puede realizar el filtrado de calidad sobre singletons inexistentes"
+	prinseq++ -fastq ./dedup/single_reads_dedup.fastq  -min_qual_mean $2 -rm_header -min_len $3 -out_good ./qual/single_reads_qual.fastq -out_bad /dev/null || echo "No se puede realizar el filtrado de calidad sobre singletons inexistentes"
 
 	cat ./qual/*singletons* >> ./qual/single_reads_qual.fastq || echo "No se recuperaron singletons en el filtro de calidad"
 
-	prinseq++ -verbose -fastq ./qual/single_reads_qual.fastq -lc_entropy=0.7 -rm_header -out_good ./compl/single_reads_compl -out_bad null || echo "No se puede realizar el filtrado de complejidad sobre singletons inexistentes"
+	prinseq++ -fastq ./qual/single_reads_qual.fastq -lc_entropy=0.7 -rm_header -out_good ./compl/single_reads_compl -out_bad null || echo "No se puede realizar el filtrado de complejidad sobre singletons inexistentes"
 
 	cat ./compl/*singletons* >> ./compl/single_reads_compl.fastq || echo "No se recuperaron singletons en el filtro de calidad"
 
-	prinseq++ -verbose -fastq ./compl/single_reads_compl.fastq -trim_qual_right=$2 -trim_qual_left=$2 -trim_qual_window 1 -trim_qual_step 1 -min_len $3 -rm_header -out_good ./trim/single_reads_trim -out_bad null || echo "No se puede realizar el trimming sobre singletons inexistentes"
+	prinseq++ -fastq ./compl/single_reads_compl.fastq -trim_qual_right=$2 -trim_qual_left=$2 -trim_qual_window 1 -trim_qual_step 1 -min_len $3 -rm_header -out_good ./trim/single_reads_trim -out_bad null || echo "No se puede realizar el trimming sobre singletons inexistentes"
 
 	bbnorm.sh in=./trim/single_reads_trim.fastq out=./clean/singletons_clean.fastq target=100 || echo "No se obtuvieron singletons durante el análisis de calidad de esta muestra"
 
-	#rm -rf dedup qual compl trim
+	rm -rf dedup qual compl trim
 
 }
 
