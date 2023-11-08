872f880c76719154a43b66da4148577e9670a390
GerardoCMM <gerardo_cendejas_mendoza@ciencias.unam.mx>
2023-11-07 19:03:33 -0600

    Updated

---
+++ annot_resume.py
@@ -0,0 +1,198 @@
+import sys
+
+from Bio import SeqIO
+
+import pandas as pd
+
+import pysam
+
+import re
+
+#import pandas as pd
+
+# Load gbk file
+
+aln_file=sys.argv[3]
+
+viruses_file=sys.argv[4]
+
+records = list(SeqIO.parse(sys.argv[1], "genbank"))
+
+# Escribir archivo fasta para todos los ORF detectados
+
+sourceFile = open(f'./{sys.argv[2]}_all.fasta', 'w')
+
+for record in records:
+    for feature in record.features:
+        if feature.type == "CDS":
+            print(f'> {feature.qualifiers["locus_tag"][0]}\n{feature.extract(record.seq)}',file=sourceFile)
+
+sourceFile.close()
+
+# Escribir archivo fasta con secuencias de AMINOACIDOS de todos los ORF detectados
+
+sourceFile = open(f'./{sys.argv[2]}_all_aa.fasta', 'w')
+
+for record in records:
+    for feature in record.features:
+        if feature.type == "CDS":
+            print(f'> {feature.qualifiers["locus_tag"][0]}\n{feature.qualifiers["translation"][0]}',file=sourceFile)
+
+sourceFile.close()
+
+# Escribir archivo fasta con secuencias con anotacion diferente a hypothetical protein
+
+sourceFile = open(f'./{sys.argv[2]}_annotated.fasta', 'w')
+
+for record in records:
+    for feature in record.features:
+        if feature.type == "CDS" and feature.qualifiers["product"][0]!="hypothetical protein":
+            print(f'> {feature.qualifiers["locus_tag"][0]}\n{feature.extract(record.seq)}',file=sourceFile)
+
+sourceFile.close()
+
+# Escribir archivo fasta con secuencias de AMINOACIDOS con anotacion diferente a hypothetical protein
+
+sourceFile = open(f'./{sys.argv[2]}_annotated_aa.fasta', 'w')
+
+for record in records:
+    for feature in record.features:
+        if feature.type == "CDS" and feature.qualifiers["product"][0]!="hypothetical protein":
+            print(f'> {feature.qualifiers["locus_tag"][0]}\n{feature.qualifiers["translation"][0]}',file=sourceFile)
+
+sourceFile.close()
+
+# Escribir archivo tabular con las anotaciones de cara transcrito y el ID de la proteína a la cuál es idéntico.
+
+p = re.compile(r'L\wC\wE')
+
+sourceFile = open(f'./{sys.argv[2]}_annotated.csv', 'w')
+
+print("Contig,LocusTag,ContigLen,LocusLen,ProteinLen,Product,Similar2,LXCXE",file=sourceFile)
+
+for record in records:
+    for feature in record.features:
+        if feature.type == "CDS" and feature.qualifiers["product"][0]!="hypothetical protein":
+            print(f'{record.name},{feature.qualifiers["locus_tag"][0]},{abs(record.features[0].location.end)},{abs(feature.location.end-feature.location.start)},{len(feature.qualifiers["translation"][0])},"{feature.qualifiers["product"][0]}",{feature.qualifiers["inference"][1].split(":")[2]},{len(p.findall(feature.qualifiers["translation"][0]))!=0}',file=sourceFile)
+
+sourceFile.close()
+
+### Creando diccionarios necesarios
+
+strand = {0:1,16:-1,256:1,272:-1,2048:1,2064:-1}
+
+aln = {0:"primary",16:"primary",256:"not primary",272:"not primary",2048:"supplementary",2064:"supplementary"}
+
+mapped = {"M":1,"D":0,"N":0,"I":2,"S":0,"H":0}
+keys = list(mapped.keys())
+
+### Definiendo función para interpretar CIGAR
+
+def get_cigar(x):
+    num = re.split(r'\D+',x)
+    tmp = re.split(r'\d',x)
+    num.remove("")
+    aligned = []
+    for i in tmp:
+        if i in keys:
+            aligned.append(i)
+            
+    final = [mapped[aligned[0]]]
+    
+    final.append(int(num[0]))
+    
+    actual = mapped[aligned[0]]
+    
+    for i in range(1,len(num)):
+        
+        if mapped[aligned[i]] == 2:
+            next        
+        if mapped[aligned[i]] == actual:
+            final[-1]+=int(num[i])
+        elif mapped[aligned[i]] != actual and int(num[i]) <= 10:
+            final[-1]+=int(num[i])
+        elif mapped[aligned[i]] != actual and int(num[i]) > 10:
+            actual = mapped[aligned[i]]
+            final.append(int(num[i]))
+            
+    final = list(map(str,final))
+            
+    return(",".join(final))
+
+def get_mapped_len(x):
+    num = re.split(r'\D+',x)
+    tmp = re.split(r'\d',x)
+    num.remove("")
+    
+    aligned = []
+    
+    for i in tmp:
+        if i in keys:
+            aligned.append(i)
+            
+    mapped_len = 0
+    
+    for i in range(0,len(num)):
+        
+        if mapped[aligned[i]] == 1:
+            mapped_len+=int(num[i])       
+        else:
+            next
+            
+    return(mapped_len)
+
+# Importando archivo en formato SAM
+
+samfile = pysam.AlignmentFile(aln_file)
+
+### Creando archivo que dice el número de  contigs alineados a cada reference genome
+
+viruses = pd.read_csv(viruses_file)
+
+viruses = viruses.set_index(viruses["ID"]) 
+
+virus_name = []
+genomes = []
+count=[]
+genome_len=[]
+coverages_len = []
+coverages_per = []
+
+for genome in samfile.get_index_statistics():
+    if genome[1]>0:
+        if genome[0].split(".")[0] in viruses.index:
+            virus_name.append(viruses.loc[genome[0].split(".")[0]]["Name"])
+        else:
+            virus_name.append(genome[0].split("|")[0])
+        genomes.append(genome[0])
+        count.append(genome[1])
+        genome_len.append(samfile.get_reference_length(genome[0]))
+        coverage = 0
+        iter = samfile.fetch(genome[0],0,samfile.get_reference_length(genome[0]))
+        for x in iter:
+            coverage+=get_mapped_len(str(x).split('\t')[5])
+        coverages_len.append(coverage)
+        coverages_per.append(coverage/samfile.get_reference_length(genome[0]))
+        
+data = {"Virus":virus_name,"Genome":genomes,"MappedContigs":count,
+        "GenomeLength":genome_len,"GenomeMappedLen":coverages_len,"GenomeCoverageIdx":coverages_per}
+data = pd.DataFrame(data,index=None)
+data=data.sort_values(by=['GenomeCoverageIdx'],ascending=False)
+
+data.to_csv(f"{sys.argv[2]}_ref_genome_maps.csv",index=False)
+
+### Creando archivo de información de alineamiento.
+
+sourceFile = open(f'./{sys.argv[2]}_aln.csv', 'w')
+
+for i in range(0,len(samfile.get_index_statistics()),1):
+    if samfile.get_reference_name(i).split(".")[0] in viruses.index:
+            name = viruses.loc[samfile.get_reference_name(i).split(".")[0]]["Name"]
+    else:
+            name = samfile.get_reference_name(i).split("|")[0]
+    ref = samfile.get_reference_name(i)
+    iter = samfile.fetch(ref,0,samfile.get_reference_length(ref))
+    for x in iter:
+        print('%s,%s,%s,%s,%s,"%s"'%(name,str(x).split('\t')[0],aln[int(str(x).split('\t')[1])],samfile.get_reference_length(ref),str(x).split('\t')[3],get_cigar(str(x).split('\t')[5])),file=sourceFile)
+
+sourceFile.close()
---
+++ functions.sh
@@ -0,0 +1,161 @@
+function CleanReads (){
+
+	# Realiza el filtrado de calidad de las reads
+
+	mkdir clean
+
+	cd clean
+
+	# Crea las carpetas necesarias
+
+	printf "\n### Quality control for $1 ###\n\n"
+
+	trimmomatic PE -threads 4 ../$1_1.fastq ../$1_2.fastq $1_1_P.fastq $1_1_U.fastq $1_2_P.fastq $1_2_U.fastq ILLUMINACLIP:$4/adapters.fa:2:30:10:2:keepBothReads SLIDINGWINDOW:4:$2 MINLEN:$3
+
+	# Archivos que tienen P son paired y U son singletons
+	# Sliding window de 4 nucleótidos y min_qual de $2 
+	# Min_len de $3
+
+	printf "\n### Getting singletons for $1 ###\n\n"
+
+	cat $1_1_U.fastq > $1_singletons.fastq
+
+	cat $1_2_U.fastq >> $1_singletons.fastq
+
+	rm *_U.fastq
+
+	cd ..
+
+}
+
+function HostMapping(){
+
+	mkdir unmapped
+
+	cd unmapped
+
+	mkdir paired singletons
+
+	cd paired
+
+	printf  "\n###Mapping paired reads of $1 to host###\n\n"
+
+	STAR --runThreadN 16 --genomeDir $2 --sjdbGTFfile $2/*.gtf --sjdbOverhang 150 -- readFilesIn ../../clean/$1_1_P.fastq ../../clean/$1_2_P.fastq --outSAMtype BAM SortedByCoordinate --outSAMstrandField intronMotif --outFileNamePrefix $1_paired --quantMode GeneCounts --outSAMunmapped Within
+
+	mv $1_paired*.bam ../$1_paired.bam
+
+	mv $1_pairedReads*.tab ../$1_paired_ReadsPerGene.tab
+
+	cd ..
+
+	samtools view -b -f 4 $1_paired.bam > $1_unmapped_paired.bam
+
+	bedtools bamtofastq -i $1_unmapped_paired.bam -fq $1_unmapped_1.fastq -fq2 $1_unmapped_2.fastq
+
+	cd ./singletons
+
+	printf  "\n###Mapping singletons of $1 to host###\n\n"
+
+	STAR --runThreadN 16 --genomeDir $2 --sjdbGTFfile $2/*.gtf --sjdbOverhang 150 -- readFilesIn ../../clean/$1_singletons.fastq --outSAMtype BAM SortedByCoordinate --outSAMstrandField intronMotif --outFileNamePrefix $1_singletons --quantMode GeneCounts --outSAMunmapped Within
+
+	mv $1_singletons*.bam ../$1_singletons.bam
+
+	mv $1_singletonsReads*.tab ../$1_singletons_ReadsPerGene.tab
+
+	cd ..
+
+	samtools view -b -f 4 $1_singletons.bam > $1_unmapped_singletons.bam
+
+	bedtools bamtofastq -i $1_unmapped_singletons.bam -fq $1_unmapped_s.fastq
+
+	rm -rf ./paired ./singletons
+
+	cd ..
+
+}
+
+function Assembly(){
+
+	printf  "\n###De novo assembly of unmapped reads of $1###\n\n"
+
+	mkdir assembly
+
+	cd assembly
+
+	rnaspades.py -1 ../unmapped/$1_unmapped_1.fastq -2 ../unmapped/$1_unmapped_2.fastq -s ../unmapped/$1_unmapped_s.fastq -k 51 -o ./$1/ --threads 8 --memory 60 -k 31,53,75,91,115
+
+	mv ./$1/transcripts.fasta ./$1_contigs.fasta
+
+	mv ./$1/*scaffolds.gfa ./$1_contigs.gfa
+
+	rm -rf ./$1
+
+	cd ..
+
+}
+
+function VirusMapping(){
+
+	printf  "\n###Mapping contigs to virus genomes###\n\n"
+
+	mkdir viralmap
+
+	cd viralmap
+
+	minimap2 -ax splice --cs -C5 -t16 $2 ../assembly/$1_contigs.fasta > $1_aln.sam
+
+	samtools view -b -F 4 $1_aln.sam > $1_mapped2virus.bam
+
+	bedtools bamtofastq -i $1_mapped2virus.bam -fq $1_mapped2virus.fastq
+
+	sed -n '1~4s/^@/>/p;2~4p' $1_mapped2virus.fastq > $1_mapped2virus.fasta
+
+	samtools sort $1_mapped2virus.bam -o $1_mapped2virus_sorted.bam
+
+	samtools index -b $1_mapped2virus_sorted.bam
+
+	rm *.fastq *.sam
+
+	rm sort $1_mapped2virus.bam
+
+	cd ..
+
+}
+
+function AnnotatingContigs(){
+
+	printf  "\n###Annotating contigs mapped to viral genomes###\n\n"
+
+	mkdir annot
+
+	cd annot
+
+	conda activate prokka_env
+
+	prokka --addgenes --outdir $1 --locustag $1 --kingdom Viruses --prefix $1_mapped2virus --metagenome --cpus 8 --norrna --notrna --centre X --compliant ../viralmap/$1_mapped2virus.fasta
+
+	conda deactivate
+
+	cd ..
+
+}
+
+function GenerateResults(){
+
+	printf  "\n###Creating results files###\n\n"
+
+	mkdir results
+
+	cd results
+
+	python $1/annot_resume.py ../annot/$2/$2_mapped2virus.gbk $2 ../viralmap/$2_mapped2virus_sorted.bam $1/virus.csv
+
+	mkdir images
+
+	cd images
+
+	Rscript --vanilla $1/Plot.R ../$2_aln.csv 
+
+	cd ../..
+
+}
