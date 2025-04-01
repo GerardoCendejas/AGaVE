37bbc868bde01967dc26365087faa84a7cf2ba53
GerardoCMM <gerardo_cendejas_mendoza@ciencias.unam.mx>
2025-04-01 12:34:52 -0600

    annot virus

--- scripts/annot_virus.py
+++ scripts/annot_virus.py
@@ -68,19 +68,19 @@ for genome in genomes:
 
                 print(f"Wrote fasta file for {genome}")
 
-        os.system(f"prokka --addgenes --outdir {outdir}prueba/ --locustag {genome} --prefix genome --kingdom Viruses --cpus {cores} --norrna --notrna \"{outdir}genome.fas\" --force")
+                os.system(f"prokka --addgenes --outdir {outdir}prueba/ --locustag {genome} --prefix genome --kingdom Viruses --cpus {cores} --norrna --notrna \"{outdir}genome.fas\" --force")
 
-        print(f"Annotated {genome}")
+                print(f"Annotated {genome}")
 
-        print(os.listdir(f"{outdir}prueba/"))
+                print(os.listdir(f"{outdir}prueba/"))
 
-        os.system(f"mv {outdir}prueba/genome.gbk {outdir}{genome}.gbk")
+                os.system(f"mv {outdir}prueba/genome.gbk {outdir}{genome}.gbk")
 
-        print(f"moved {genome} gbk")
+                print(f"moved {genome} gbk")
 
-        os.system(f"mv {outdir}genome.fas {outdir}{genome}.fas")
+                os.system(f"mv {outdir}genome.fas {outdir}{genome}.fas")
 
-        print(f"moved {genome} fas")
+                print(f"moved {genome} fas")
 
         #os.system(f"rm -rf {outdir}prueba/")
 
