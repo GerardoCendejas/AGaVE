cfb503f0d7d24dac9a8d960080e6434f37d7f5aa
GerardoCMM <gerardo_cendejas_mendoza@ciencias.unam.mx>
2025-04-08 11:29:01 -0600

    telescope

--- scripts/annot_virus.py
+++ scripts/annot_virus.py
@@ -96,7 +96,7 @@ for genome in genomes:
                             ID_tag = record.id.replace("|", "_")
                             locus_tag = str(feature.qualifiers["locus_tag"][0]).replace("|", "_")
 
-                            print(f'{ID_tag}\tprokka\tCDS\t{feature.location.start}\t{feature.location.end}\t.\t{feature.strand}\t.\tID {locus_tag};Name {gene};locus {locus_tag};',file=sourceFile)
+                            print(f'{ID_tag}\tprokka\tCDS\t{feature.location.start}\t{feature.location.end}\t.\t{feature.strand}\t.\tID \"{locus_tag}\";Name \"{gene}\";locus \"{locus_tag}\";',file=sourceFile)
 
                 sourceFile.close()
 
