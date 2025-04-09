2aa44f2850f8f271b010231c7704b158eb0e3111
GerardoCMM <gerardo_cendejas_mendoza@ciencias.unam.mx>
2025-04-09 15:26:57 -0600

    telescope

--- scripts/annot_virus.py
+++ scripts/annot_virus.py
@@ -96,16 +96,11 @@ for genome in genomes:
                             ID_tag = "_".join(str(feature.qualifiers["locus_tag"][0]).split("_")[0:-1]).strip('"')
                             locus_tag = str(feature.qualifiers["locus_tag"][0]).strip('"')
 
-                            attributes = (
-                                f'gene_id "{locus_tag}"; '
-                                f'transcript_id "{locus_tag}"; '
-                                f'locus "{locus_tag}"; '
-                                f'product "{gene}";'
-                            )
+                            attributes = f'gene_id "{locus_tag}"; transcript_id "{locus_tag}"; locus "{locus_tag}"; product "{gene}";'
 
                             strand = "+" if feature.strand == 1 else "-" if feature.strand == -1 else "."
 
-                            line = f'{ID_tag}\tprokka\texon\t{feature.location.start}\t{feature.location.end}\t.\t{strand}\t.\t{attributes}'
+                            line = f'{ID_tag}\tprokka\texon\t{feature.location.start}\t{feature.location.end}\t.\t{strand}\t.\t{attributes}\n'
                             sourceFile.write(line)
 
                 sourceFile.close()
