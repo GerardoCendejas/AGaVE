dd8d76d61bd5ea2ffa193b44b025e2b2b78aa09a
GerardoCMM <gerardo_cendejas_mendoza@ciencias.unam.mx>
2025-04-09 14:20:04 -0600

    telescope

--- scripts/annot_virus.py
+++ scripts/annot_virus.py
@@ -93,7 +93,7 @@ for genome in genomes:
                             except:
                                 gene = feature.qualifiers["product"][0].split(",")[0]
 
-                            ID_tag = str(feature.qualifiers["locus_tag"][0]).split("_")[0]
+                            ID_tag = str(feature.qualifiers["locus_tag"][0]).split("_")[0:-1]
                             locus_tag = str(feature.qualifiers["locus_tag"][0])
 
                             attributes = (
